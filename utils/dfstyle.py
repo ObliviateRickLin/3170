5f1eda94bf56d68736f11071b880c6acca584dfb
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-23 15:39:58 +0800

    Update dfstyle.py

--- utils/dfstyle.py
+++ utils/dfstyle.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def color_zero_red(val):
+def color_zero_green(val):
     """
     Takes a scalar and returns a string with
     the css property `'color: red'` for zero
     strings, white otherwise.
     """
-    color = 'red' if val == 0 else 'white'
+    color = 'green' if val == 0 else 'white'
     return 'color: %s' % color
 
