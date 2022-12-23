d419e5df1e89265cead9f3fd4ed7674dda788a9c
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-23 15:16:23 +0800

    Update shopping_cart_page.py

--- pagelib/consumer/shopping_cart_page.py
+++ pagelib/consumer/shopping_cart_page.py
@@ -9,15 +9,15 @@ def shopping_cart_page():
     # title
     st.title('Chip Shopping Cart')
     st.write("___________________________________")
-    for _, row in chip_type.iterrows():
+    for index, row in chip_type.iterrows():
         with st.container():
             c0, c1, c2, c3, c4 = st.columns((1,5,1.4,2,1.8))
             with c0: display_icon(cpu)
             with c1: st.subheader(row["CHIP_NAME"]);st.caption("Version: %s"%row["CHIP_VERSION"])
             with c2: st.subheader("%s$"%row["PRICE"])
             with c3: st.selectbox("Selectable Plants", options=("Plant A", "Plant B","Platn C"), key = ("plant",row["CHIP_NAME"],row["CHIP_VERSION"]))
-            row["NUMBER"] = c4.number_input(label = "Number", value=0, step=1, min_value=0, key = ("version",row["CHIP_NAME"],row["CHIP_VERSION"]))
-            row["COST"] = row["NUMBER"] * row["PRICE"]
+            chip_type.loc[index,"NUMBER"] = c4.number_input(label = "Number", value=0, step=1, min_value=0, key = ("version",row["CHIP_NAME"],row["CHIP_VERSION"]))
+            chip_type["COST"] = chip_type["NUMBER"] * chip_type["PRICE"]
             st.write("___________________________________")
     total = chip_type["COST"].sum()
     st.subheader(total)
