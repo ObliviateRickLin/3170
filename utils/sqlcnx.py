fec427d2e3ffa219a04d6bf1afeea93a20ffc2b7
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-23 20:15:50 +0800

    Update sqlcnx.py

--- utils/sqlcnx.py
+++ utils/sqlcnx.py
@@ -18,22 +18,15 @@ def run_query(query):
     
 # Fetch one result
 def get_chip_type():
-    cnx = mysql.connector.connect(
-        host="123.60.157.95",
-        port=3306,
-        user="root",
-        password="csc123456@",
-        database="project")
-    cur = cnx.cursor()
-    cur.execute("""
-            SELECT *  
-            FROM chip_type;
-            """)
-    chip_type = pd.DataFrame(cur.fetchall())
+    chip_type_sql = run_query("""
+                                SELECT *  
+                                FROM chip_type;
+                                """)
+    chip_type = pd.DataFrame(chip_type_sql)
     chip_type.columns = ["CHIP_NAME", "CHIP_VERSION", "PRICE"]
     chip_type["NUMBER"] = 0
     chip_type["COST"] = 0
-    cnx.close()     
+   
     return chip_type
 
 
