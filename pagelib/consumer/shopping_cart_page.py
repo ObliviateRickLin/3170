1ca8ca96565beb2b179d6cdefdc7a861a9352ecc
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 15:34:24 +0800

    change para

--- pagelib/consumer/shopping_cart_page.py
+++ pagelib/consumer/shopping_cart_page.py
@@ -49,11 +49,7 @@ def shopping_cart_page():
             with st.expander("Check your shopping cart information"):
                 st.dataframe(chip_type.style.applymap(color_zero))
             ddl = datetime.combine(ddl, datetime.now().time())
-            package_info = (str(st.session_state["ID"]), 3, str(total), today.strftime("%Y-%m-%d %H:%M:%S"), ddl.strftime("%Y-%m-%d %H:%M:%S"))
-            st.write(type(st.session_state["ID"]))
-            st.write(str(total))
-            st.write(type(today.strftime("%Y-%m-%d %H:%M:%S")))
-            st.write(type(ddl.strftime("%Y-%m-%d %H:%M:%S")))
+            package_info = (st.session_state["ID"], str(total), today.strftime("%Y-%m-%d %H:%M:%S"), ddl.strftime("%Y-%m-%d %H:%M:%S"))
     if st.button("SUBMIT YOUR PACKAGE"):
         cnx = mysql.connector.connect(
                 host="123.60.157.95",
@@ -62,7 +58,7 @@ def shopping_cart_page():
                 password="csc123456@",
                 database="project") 
         cur = cnx.cursor()
-        query1 = """INSERT INTO package (USER_ID, PACKAGE_ID, BUDGET, CREATE_TIME, DEADLINE) VALUES (%s, %s, %s, %s, %s)"""
+        query1 = """INSERT INTO package (USER_ID,  BUDGET, CREATE_TIME, DEADLINE) VALUES (%s, %s, %s, %s)"""
         cur.execute(query1, package_info)
         cnx.commit()
         cnx.close()
