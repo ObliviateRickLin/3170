705b673bf4ec214306a402e2ddc7699f42e8af3f
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 17:35:18 +0800

    Update log_page.py

--- pagelib/log_page.py
+++ pagelib/log_page.py
@@ -15,34 +15,35 @@ def log_in_based_on_info(role):
     return
     
 def log_in_page():
-    cnx = mysql.connector.connect(
-        host="123.60.157.95",
-        port=3306,
-        user="root",
-        password="csc123456@",
-        database="project") 
-    first_name = st.text_input("First name",placeholder="Yangsheng")
-    last_name = st.text_input("Last name",placeholder="Xu")
-    password = st.text_input("Password",placeholder="1999GJ5")
+    with st.form("LOG IN"):
+        cnx = mysql.connector.connect(
+            host="123.60.157.95",
+            port=3306,
+            user="root",
+            password="csc123456@",
+            database="project") 
+        first_name = st.text_input("First name",placeholder="Yangsheng")
+        last_name = st.text_input("Last name",placeholder="Xu")
+        password = st.text_input("Password",placeholder="1999GJ5")
 
-    # Get a cursor
-    cur = cnx.cursor()
-    # Execute a query
-    cur.execute("""
-                SELECT ROLE, USER_ID  
-                FROM user
-                WHERE (FIRST_NAME = %s)
-                AND (LAST_NAME = %s)
-                AND (PASSWORD = %s);
-                """, (first_name,last_name,password))
-    result = cur.fetchone()
-    if result:
-        role, ID= result
-    elif result == None:
-        role = result
+        # Get a cursor
+        cur = cnx.cursor()
+        # Execute a query
+        cur.execute("""
+                    SELECT ROLE, USER_ID  
+                    FROM user
+                    WHERE (FIRST_NAME = %s)
+                    AND (LAST_NAME = %s)
+                    AND (PASSWORD = %s);
+                    """, (first_name,last_name,password))
+        result = cur.fetchone()
+        if result:
+            role, ID= result
+        elif result == None:
+            role = result
 
     # Fetch one result
-    if st.button("Log In"):
+    if st.form_submit_button("Log In"):
         if role == None:
             st.warning("INVALID INFORMATION! RECHECK YOUR INPUT!")
         elif role == "staff":
