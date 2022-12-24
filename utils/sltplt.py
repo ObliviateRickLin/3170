bbbb1d75aec30375ee23b64bb5598d76cc0a8f21
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 13:27:04 +0800

    change

--- utils/sltplt.py
+++ utils/sltplt.py
@@ -75,8 +75,8 @@ def select_plants_with_chip():
     plants = [plant_[0] for plant_ in plants_]
     chip_type = pd.DataFrame(run_query("SELECT CHIP_NAME, CHIP_VERSION FROM chip_type"))
     chip_type.columns =["CHIP_NAME", "CHIP_VERSION"]
-    chip_type["Selected_plants"] = 0
-    chip_type["Selected_plants"] = chip_type.apply(lambda row: selectable_plants(plants, row["chip_name"], row['chip_version']), axis=1)
+    chip_type["PLANTS"] = 0
+    chip_type["PLATNS"] = chip_type.apply(lambda row: selectable_plants(plants, row["CHIP_NAME"], row['CHIP_VERSION']), axis=1)
     return chip_type
 
 if __name__ == "__main__":
