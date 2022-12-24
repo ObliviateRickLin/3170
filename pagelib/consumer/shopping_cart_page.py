6e26f4d3a8640849679ff72d0e6607c60ba36b38
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 13:47:25 +0800

    Update shopping_cart_page.py

--- pagelib/consumer/shopping_cart_page.py
+++ pagelib/consumer/shopping_cart_page.py
@@ -22,29 +22,31 @@ def shopping_cart_page():
             st.session_state["chip"] = select_plants_with_chip()
     chip_type = get_chip_type()
     chip_type = chip_type.merge(st.session_state["chip"], on=["CHIP_NAME","CHIP_VERSION"])
-
-    for index, row in chip_type.iterrows():
-        with st.container():
-            c0, c1, c2, c3, c4 = st.columns((1,5,1.4,2,1.8))
-            with c0: display_icon(cpu)
-            with c1: st.subheader(row["CHIP_NAME"]);st.caption("Version: %s"%row["CHIP_VERSION"])
-            with c2: st.subheader("%s$"%row["PRICE"])
-            with c3: chip_type.loc[index,"PLANTS"] = st.selectbox("Selected_plants", options=(row["PLANTS"]), key = ("plant",row["CHIP_NAME"],row["CHIP_VERSION"]))
-            chip_type.loc[index,"NUMBER"] = c4.number_input(label = "Number", value=0, step=1, min_value=0, key = ("version",row["CHIP_NAME"],row["CHIP_VERSION"]))
-            chip_type["COST"] = chip_type["NUMBER"] * chip_type["PRICE"]
-            st.write("___________________________________")
-    total = chip_type["COST"].sum()
-    c1, c2 = st.columns((1,2))
-    with c1: 
-        st.metric(label="TOTAL COST", value = total) 
-    with c2: 
-        st.date_input("Choose a DDL of your package", 
-                value = dft_date,
-                min_value = min_date, 
-                max_value = max_date, 
-                help="DDL should be about 10 to 20 days after today.")
-    with st.expander("Check your shopping cart information"):
-        st.dataframe(chip_type.style.applymap(color_zero))
+    with st.form():
+        for index, row in chip_type.iterrows():
+            with st.container():
+                c0, c1, c2, c3, c4 = st.columns((1,5,1.4,2,1.8))
+                with c0: display_icon(cpu)
+                with c1: st.subheader(row["CHIP_NAME"]);st.caption("Version: %s"%row["CHIP_VERSION"])
+                with c2: st.subheader("%s$"%row["PRICE"])
+                with c3: chip_type.loc[index,"PLANTS"] = st.selectbox("Selected_plants", options=(row["PLANTS"]), key = ("plant",row["CHIP_NAME"],row["CHIP_VERSION"]))
+                chip_type.loc[index,"NUMBER"] = c4.number_input(label = "Number", value=0, step=1, min_value=0, key = ("version",row["CHIP_NAME"],row["CHIP_VERSION"]))
+                chip_type["COST"] = chip_type["NUMBER"] * chip_type["PRICE"]
+                st.write("___________________________________")
+        submitted = st.form_submit_button("Check your package")
+        if submitted:
+            total = chip_type["COST"].sum() 
+            c1, c2 = st.columns((1,2))
+            with c1: 
+                st.metric(label="TOTAL COST", value = total) 
+            with c2: 
+                st.date_input("Choose a DDL of your package", 
+                        value = dft_date,
+                        min_value = min_date, 
+                        max_value = max_date, 
+                        help="DDL should be about 10 to 20 days after today.")
+            with st.expander("Check your shopping cart information"):
+                st.dataframe(chip_type.style.applymap(color_zero))
     #st.write(product_quantities)
     return 
 
