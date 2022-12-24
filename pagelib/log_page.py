c406de186f21f31a175f042dcbe138a10fb5c532
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 17:39:11 +0800

    Update log_page.py

--- pagelib/log_page.py
+++ pagelib/log_page.py
@@ -15,51 +15,50 @@ def log_in_based_on_info(role):
     return
     
 def log_in_page():
-    with st.form("LOG IN"):
-        cnx = mysql.connector.connect(
-            host="123.60.157.95",
-            port=3306,
-            user="root",
-            password="csc123456@",
-            database="project") 
-        first_name = st.text_input("First name",placeholder="Yangsheng")
-        last_name = st.text_input("Last name",placeholder="Xu")
-        password = st.text_input("Password",placeholder="1999GJ5")
+    cnx = mysql.connector.connect(
+        host="123.60.157.95",
+        port=3306,
+        user="root",
+        password="csc123456@",
+        database="project") 
+    first_name = st.text_input("First name",placeholder="Yangsheng")
+    last_name = st.text_input("Last name",placeholder="Xu")
+    password = st.text_input("Password",placeholder="1999GJ5")
 
-        # Get a cursor
-        cur = cnx.cursor()
-        # Execute a query
-        cur.execute("""
-                    SELECT ROLE, USER_ID  
-                    FROM user
-                    WHERE (FIRST_NAME = %s)
-                    AND (LAST_NAME = %s)
-                    AND (PASSWORD = %s);
-                    """, (first_name,last_name,password))
-        result = cur.fetchone()
-        if result:
-            role, ID= result
-        elif result == None:
-            role = result
+    # Get a cursor
+    cur = cnx.cursor()
+    # Execute a query
+    cur.execute("""
+                SELECT ROLE, USER_ID  
+                FROM user
+                WHERE (FIRST_NAME = %s)
+                AND (LAST_NAME = %s)
+                AND (PASSWORD = %s);
+                """, (first_name,last_name,password))
+    result = cur.fetchone()
+    if result:
+        role, ID= result
+    elif result == None:
+        role = result
 
     # Fetch one result
-        if st.form_submit_button("Log In"):
-            if role == None:
-                st.warning("INVALID INFORMATION! RECHECK YOUR INPUT!")
-            elif role == "staff":
-                move_to_staff_state()
-                if "ID" not in st.session_state: 
-                    st.session_state["ID"] = 0
-                st.session_state["ID"] = ID
-                st.success("Log in Seccessfully as staff. Please click the button again.")
-                st.balloons()
-            elif role == "consumer":
-                move_to_consumer_state()
-                if "ID" not in st.session_state: 
-                    st.session_state["ID"] = 0
-                st.session_state["ID"] = ID
-                st.success("Log in Seccessfully as consumer. Please click the button again.")
-                st.balloons()
+    if st.button("Log In"):
+        if role == None:
+            st.warning("INVALID INFORMATION! RECHECK YOUR INPUT!")
+        elif role == "staff":
+            move_to_staff_state()
+            if "ID" not in st.session_state: 
+                st.session_state["ID"] = 0
+            st.session_state["ID"] = ID
+            st.success("Log in Seccessfully as staff. Please click the button again.")
+            st.balloons()
+        elif role == "consumer":
+            move_to_consumer_state()
+            if "ID" not in st.session_state: 
+                st.session_state["ID"] = 0
+            st.session_state["ID"] = ID
+            st.success("Log in Seccessfully as consumer. Please click the button again.")
+            st.balloons()
     cnx.close()
 
 def sign_up_page():
