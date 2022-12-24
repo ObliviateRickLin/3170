6dc6bd977ea17fbb2f3e796c554ef9a9f6b0b5e9
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-24 17:47:24 +0800

    Update log_page.py

--- pagelib/log_page.py
+++ pagelib/log_page.py
@@ -62,42 +62,41 @@ def log_in_page():
     cnx.close()
 
 def sign_up_page():
-    with st.form("SIGN IN"):
-        cnx = mysql.connector.connect(
-        host="123.60.157.95",
-        port=3306,
-        user="root",
-        password="csc123456@",
-        database="project") 
-        cur = cnx.cursor()
-        #user_id = st.text_input("User ID")
-        first_name = st.text_input("First Name")
-        last_name = st.text_input("Last Name")
-        phone_number = st.text_input("Phone Number")
+    cnx = mysql.connector.connect(
+    host="123.60.157.95",
+    port=3306,
+    user="root",
+    password="csc123456@",
+    database="project") 
+    cur = cnx.cursor()
+    #user_id = st.text_input("User ID")
+    first_name = st.text_input("First Name")
+    last_name = st.text_input("Last Name")
+    phone_number = st.text_input("Phone Number")
 
-        # Create a selectbox for countries
-        country_name = st.text_input("Input your country:")
-        # Create a selectbox for provinces
-        if country_name == "China":
-            province_options =  ["Anhui", "Beijing", "Chongqing", "Fujian", "Gansu", "Guangdong", "Guangxi", "Guizhou", "Hainan", "Hebei", "Heilongjiang", "Henan", "Hubei", "Hunan", "Jiangsu", "Jiangxi", "Jilin", "Liaoning", "Inner Mongolia", "Ningxia", "Qinghai", "Shaanxi", "Shandong", "Shanghai", 
-                            "Shanxi", "Sichuan", "Tianjin", "Tibet", "Xinjiang", "Yunnan", "Zhejiang", 
-                            "Taiwan", "Hong Kong", "Macau"]
-            province = st.selectbox("Select a province:", province_options)
-        else:
-            province = st.selectbox("Select a province:", ["International",])
-        # Print the selection
+    # Create a selectbox for countries
+    country_name = st.text_input("Input your country:")
+    # Create a selectbox for provinces
+    if country_name == "China":
+        province_options =  ["Anhui", "Beijing", "Chongqing", "Fujian", "Gansu", "Guangdong", "Guangxi", "Guizhou", "Hainan", "Hebei", "Heilongjiang", "Henan", "Hubei", "Hunan", "Jiangsu", "Jiangxi", "Jilin", "Liaoning", "Inner Mongolia", "Ningxia", "Qinghai", "Shaanxi", "Shandong", "Shanghai", 
+                        "Shanxi", "Sichuan", "Tianjin", "Tibet", "Xinjiang", "Yunnan", "Zhejiang", 
+                        "Taiwan", "Hong Kong", "Macau"]
+        province = st.selectbox("Select a province:", province_options)
+    else:
+        province = st.selectbox("Select a province:", ["International",])
+    # Print the selection
 
-        role = st.selectbox("Role", ["consumer", "staff"])
-        street_address = st.text_input("Street Address")
-        password = st.text_input("Password")
+    role = st.selectbox("Role", ["consumer", "staff"])
+    street_address = st.text_input("Street Address")
+    password = st.text_input("Password")
 
-        if st.form_submit_button("Sign Up"):
-            query = """
-            INSERT INTO user (FIRST_NAME, LAST_NAME, PHONE_NUMBER, COUNTRY_NAME, PROVINCE, ROLE, STREET_ADDRESS, PASSWORD)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            cur.execute(query, (first_name, last_name, phone_number, country_name, province, role, street_address, password))
-            cnx.commit()
-            st.success("Sign Up Seccessfully")
-            st.snow()
-        cnx.close()
+    if st.button("Sign Up"):
+        query = """
+        INSERT INTO user (FIRST_NAME, LAST_NAME, PHONE_NUMBER, COUNTRY_NAME, PROVINCE, ROLE, STREET_ADDRESS, PASSWORD)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cur.execute(query, (first_name, last_name, phone_number, country_name, province, role, street_address, password))
+        cnx.commit()
+        st.success("Sign Up Seccessfully")
+        st.snow()
+    cnx.close()
