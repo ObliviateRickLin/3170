11e2a8c4df4d19d77fb5498d066b0f6dd97efd69
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-25 23:16:44 +0800

    Update shopping_cart_page.py

--- pagelib/consumer/shopping_cart_page.py
+++ pagelib/consumer/shopping_cart_page.py
@@ -59,54 +59,56 @@ def shopping_cart_page():
             st.session_state["package"] = (st.session_state["ID"], str(total), today.strftime("%Y-%m-%d %H:%M:%S"), ddl.strftime("%Y-%m-%d %H:%M:%S"))
     
     if st.button("SUBMIT YOUR PACKAGE"):
-        package_info = st.session_state["package"]
-        plant_info = st.session_state["plant_info"]
-        cnx = mysql.connector.connect(
-                host="123.60.157.95",
-                port=3306,
-                user="root",
-                password="csc123456@",
-                database="project") 
-        cur = cnx.cursor()
-        query1 = """INSERT INTO package (USER_ID,  BUDGET, CREATE_TIME, DEADLINE) VALUES (%s, %s, %s, %s)"""
-        cur.execute(query1, package_info)
-        cnx.commit()
-        cur2 = cnx.cursor()
-        cur2.execute("SELECT LAST_INSERT_ID()")
-        package_id, = cur2.fetchone()
-        cnx.close()
-        package_info = st.session_state["package"]
-        plant_info = st.session_state["plant_info"]
-        cnx = mysql.connector.connect(
-                host="123.60.157.95",
-                port=3306,
-                user="root",
-                password="csc123456@",
-                database="project") 
-        cur = cnx.cursor()
-        query = """INSERT INTO state (PACKAGE_ID, START_TIME, END_TIME, STATE_NAME) VALUES (%s, %s, %s, %s)"""
-        cur.execute(query, (package_id, package_info[2], package_info[3], "Waiting"))
-        cnx.commit()
-        cnx.close()
-        cnx = mysql.connector.connect(
-                host="123.60.157.95",
-                port=3306,
-                user="root",
-                password="csc123456@",
-                database="project") 
-        cur = cnx.cursor()
-        query3 = """INSERT INTO chip (CHIP_NAME, CHIP_VERSION, PRICE, PACKAGE_ID, PLANT_ID) VALUES (%s, %s,%s, %s, %s)"""
-        chip_in_package = []
-        for index, row in chip_type.iterrows():
-            num = row["NUMBER"]
-            for _ in range(num):
-                chip_in_package.append((row["CHIP_NAME"],row["CHIP_VERSION"],row["PRICE"],package_id, plant_info[row["PLANTS"]]))
-        cur.executemany(query3, chip_in_package)
-        cnx.commit()
-        cnx.close()
-        st.success("Subbmit successfully")
-        st.experimental_rerun()
-
+        try:
+            package_info = st.session_state["package"]
+            plant_info = st.session_state["plant_info"]
+            cnx = mysql.connector.connect(
+                    host="123.60.157.95",
+                    port=3306,
+                    user="root",
+                    password="csc123456@",
+                    database="project") 
+            cur = cnx.cursor()
+            query1 = """INSERT INTO package (USER_ID,  BUDGET, CREATE_TIME, DEADLINE) VALUES (%s, %s, %s, %s)"""
+            cur.execute(query1, package_info)
+            cnx.commit()
+            cur2 = cnx.cursor()
+            cur2.execute("SELECT LAST_INSERT_ID()")
+            package_id, = cur2.fetchone()
+            cnx.close()
+            package_info = st.session_state["package"]
+            plant_info = st.session_state["plant_info"]
+            cnx = mysql.connector.connect(
+                    host="123.60.157.95",
+                    port=3306,
+                    user="root",
+                    password="csc123456@",
+                    database="project") 
+            cur = cnx.cursor()
+            query = """INSERT INTO state (PACKAGE_ID, START_TIME, END_TIME, STATE_NAME) VALUES (%s, %s, %s, %s)"""
+            cur.execute(query, (package_id, package_info[2], package_info[3], "Waiting"))
+            cnx.commit()
+            cnx.close()
+            cnx = mysql.connector.connect(
+                    host="123.60.157.95",
+                    port=3306,
+                    user="root",
+                    password="csc123456@",
+                    database="project") 
+            cur = cnx.cursor()
+            query3 = """INSERT INTO chip (CHIP_NAME, CHIP_VERSION, PRICE, PACKAGE_ID, PLANT_ID) VALUES (%s, %s,%s, %s, %s)"""
+            chip_in_package = []
+            for index, row in chip_type.iterrows():
+                num = row["NUMBER"]
+                for _ in range(num):
+                    chip_in_package.append((row["CHIP_NAME"],row["CHIP_VERSION"],row["PRICE"],package_id, plant_info[row["PLANTS"]]))
+            cur.executemany(query3, chip_in_package)
+            cnx.commit()
+            cnx.close()
+            st.success("Subbmit successfully")
+            st.experimental_rerun()
+        except:
+            st.warning("Please check your package first before submitting!")
 
     #st.write(product_quantities)
     return 
