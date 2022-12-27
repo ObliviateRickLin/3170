a846cd016e60b006bfac0c6db9734621bda8c39f
120090005 <118040302+120090005@users.noreply.github.com>
2022-12-27 20:41:52 +0800

    Update plant_management.py

--- pagelib/staff/plant_management.py
+++ pagelib/staff/plant_management.py
@@ -77,6 +77,20 @@ def plant_management_sys():
                             """%chip)
                 cnx3.commit()
                 cnx3.close()
+                for i in selected:
+                cnx6 = ms.connect(
+                    host="123.60.157.95",
+                    port=3306,
+                    user="root",
+                    password="csc123456@",
+                    database="project")
+                cur6 = cnx6.cursor()
+                cur6.execute("""select chip_id
+                                from chip
+                                where chip_name = %s
+                """%i)
+                chip_id = cur6.fetchone()
+                allocate_task(chip_id)
                 st.text("Successful Management!\nPlease Reflash This Page")
         
     with tab2:
@@ -98,3 +112,84 @@ def plant_management_sys():
             columns=["Chip Name", "State"])
         cnx4.close()
         st.table(df2)
+
+def run_query(query, *args):
+    cnx = mysql.connector.connect(
+        host="123.60.157.95",
+        port=3306,
+        user="root",
+        password="csc123456@",
+        database="project")
+    cnx.reconnect()
+    with cnx.cursor(buffered=True) as cur:
+        cur.execute(query,*args)
+        result = cur.fetchall()
+    cnx.close()
+    return result
+
+
+def generate_operation(machine_id, operation_name, chip_id):
+    cnx = mysql.connector.connect(
+        host="123.60.157.95",
+        port=3306,
+        user="root",
+        password="csc123456@",
+        database="project")
+
+    # Write an operation
+    start_time = str(datetime.datetime.now())
+    cur = cnx.cursor()
+    cur.execute("INSERT INTO operation(operation_name, start_time, end_time) VALUES (%s, %s, NULL);", (operation_name, start_time))
+    cnx.commit()
+
+    # Derive the operation id
+    cur2 = cnx.cursor()
+    cur2.execute("SELECT LAST_INSERT_ID();")
+    operation_id = cur2.fetchone()[0]
+
+
+    """
+    # Write a relation between chip and operation
+    cur3 = cnx.cursor()
+    cur3.execute("INSERT INTO operation_with_chip(operation_id, chip_id) VALUES (%s, %s);", (operation_id, chip_id))
+    cnx.commit()
+    """
+
+    # Write a relation between machine and operation
+    cur4 = cnx.cursor()
+    cur4.execute("INSERT INTO operation_with_machine(operation_id, machine_id) VALUES (%s, %s);", (operation_id, machine_id))
+    cnx.commit()
+
+    cnx.close()
+
+
+def allocate_task(chip_id):
+
+    cnx = mysql.connector.connect(
+        host="123.60.157.95",
+        port=3306,
+        user="root",
+        password="csc123456@",
+        database="project")
+    # Find chip_name and chip_version
+    chip_info = run_query("SELECT chip_name, chip_version FROM chip WHERE chip_id = %s;", (chip_id))
+    chip_name, chip_version = chip_info[0][0], chip_info[0][1]
+
+
+    # Find all operation type needed
+    first_order_operation_name = run_query("SELECT operation_name FROM chip_type_with_operation_type WHERE chip_name = %s AND chip_version = %s AND order_op = 0;", (chip_name, chip_version))[0][0]
+
+
+    # Find all machines in the plant of the chip
+    plant_id = run_query("SELECT plant_id FROM chip WHERE chip_id = %s;", (chip_id))[0]
+    machines = run_query("SELECT machine_id, machine_name, machine_version FROM machine WHERE plant_id = %s;", (plant_id))
+
+    # Find suitable machines which could process the corresponding operation type
+    for machine in machines:
+        operation_names = run_query("SELECT operation_name FROM machine_type_with_operation_type WHERE machine_name = %s AND machine_version = %s;", (machine[1], machine[2]))
+        operation_names = [i[0] for i in operation_names]
+        if first_order_operation_name in operation_names:
+            # Need to do: if machine is free, then allocate to the machine
+            generate_operation(machine[0], first_order_operation_name, chip_id[0])
+            # Need to do: set the state of machine as `busy`
+            break
